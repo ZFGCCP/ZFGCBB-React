0afb4a69a998f166299a0ce092e1845028d4aaa3
steve <onisephiroth@gmail.com>
2024-09-27 00:05:06 -0400

    updates for user profile and thread

---
+++ src/pages/forum/thread/[threadId].tsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import ForumThread from "../../../components/forum/boards/thread.component";
+
+const UserProfile: React.FC = () => {
+  return (
+    <>
+      <ForumThread/>
+    </>
+  );
+};
+
+export default UserProfile;
---
+++ src/pages/user/profile/[userId].tsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import Widget from "../../../components/common/widgets/widget.component";
+import UserProfileMaster from "../../../components/user/userProfileMaster.component";
+
+const UserProfile: React.FC = () => {
+  return (
+    <>
+      <UserProfileMaster />
+    </>
+  );
+};
+
+export default UserProfile;
