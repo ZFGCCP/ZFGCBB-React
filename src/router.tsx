a18d3d77964031991417e6dd051cc1d143d20955
Jonathan Basniak <jon@bluedreamers.com>
2024-09-29 20:20:25 -0400

    feat(frontend): add convention for exporting a layout chore: remove deprecated reactRefresh vite plugin

--- src/router.tsx
+++ src/router.tsx
@@ -1,39 +1,46 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 const ContentView = lazy(() => import("./components/contentView.component"));
 
-const imported_routes = import.meta.glob("./pages/**/*.tsx", {
-  import: "default",
-});
-const regex_home_route_matches = /\.\/pages\/home.tsx$/;
+export type BBPage = {
+  default: React.FC;
+  layout?: React.FC;
+}
+
+const imported_pages = import.meta.glob("./pages/**/*.tsx") as Record<string, () => Promise<BBPage>>;
+
 const regex_route_start_matches = /\.\//;
 const regex_route_path_matches =
   /\/(src|pages|index|home|components|routes)|(\.tsx|\.component.tsx)$/g;
 const regex_slug_matches = /\[\.{3}.+\]/;
 const regex_slug_value_matches = /\[(.+)\]/;
 
-async function lazyLoad(path: string) {
-  const Component = (await imported_routes[path]()) as React.FC;
-  return { Component };
+async function lazyLoadPage(path: string) {
+  const { default: Component, layout } = (await imported_pages[path]());
+  return { Component, layout };
 }
 
-const routes = Object.keys(imported_routes).map((route) => {
-  if (regex_home_route_matches.test(route))
-    return {
-      element: <ContentView />,
-      children: [{ path: "/", lazy: () => lazyLoad(route) }],
-    };
-  const path = route
+async function lazyLoadPageWithLayout(path: string) {
+  const { layout } = (await imported_pages[path]());
+  if (!layout) return import("./components/contentView.component");
+
+  return { default: layout! };
+}
+
+const routes = Object.keys(imported_pages).map((page) => {
+  const path = page
     .replace(regex_route_start_matches, "/")
     .replace(regex_route_path_matches, "")
     .replace(regex_slug_matches, "*")
     .replace(regex_slug_value_matches, ":$1");
 
-  //return { path, lazy: () => lazyLoad(route) };
+  const LayoutElement = lazy(() => lazyLoadPageWithLayout(page));
   return {
-    element: <ContentView />,
-    children: [{ path: path, lazy: () => lazyLoad(route) }],
+    element: <Suspense fallback={<div>Loading...</div>}><LayoutElement /></Suspense>,
+    children: [{
+      path: path, lazy: () => lazyLoadPage(page),
+    }],
   };
 });
 
