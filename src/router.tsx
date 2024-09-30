af7999bab9d9be9e2775e84bc3bd23dadebe765c
Jonathan Basniak <jon@bluedreamers.com>
2024-09-29 20:26:07 -0400

    chore: formatting and convention changes

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,6 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-const ContentView = lazy(() => import("./components/contentView.component"));
-
 export type BBPage = {
   default: React.FC;
   layout?: React.FC;
@@ -13,30 +11,30 @@ const imported_pages = import.meta.glob("./pages/**/*.tsx") as Record<
   () => Promise<BBPage>
 >;
 
-const regex_route_start_matches = /\.\//;
-const regex_route_path_matches =
-  /\/(src|pages|index|home|components|routes)|(\.tsx|\.component.tsx)$/g;
-const regex_slug_matches = /\[\.{3}.+\]/;
-const regex_slug_value_matches = /\[(.+)\]/;
-
 async function lazyLoadPage(path: string) {
   const { default: Component, layout } = await imported_pages[path]();
   return { Component, layout };
 }
 
 async function lazyLoadPageWithLayout(path: string) {
-  const { layout } = await imported_pages[path]();
+  const { layout } = await lazyLoadPage(path);
   if (!layout) return import("./components/contentView.component");
 
   return { default: layout! };
 }
 
+const regexRouteMatchStartPath = /\.\//;
+const regexRouteMatchComponentName =
+  /\/(src|pages|index|home|components|routes)|(\.tsx|\.component.tsx)$/g;
+const regexRouteMatchTripleDotSlug = /\[\.{3}.+\]/;
+const regexRouteMatchSlugName = /\[(.+)\]/;
+
 const routes = Object.keys(imported_pages).map((page) => {
   const path = page
-    .replace(regex_route_start_matches, "/")
-    .replace(regex_route_path_matches, "")
-    .replace(regex_slug_matches, "*")
-    .replace(regex_slug_value_matches, ":$1");
+    .replace(regexRouteMatchStartPath, "/") // replaces the ./ at the start of the path with a /
+    .replace(regexRouteMatchComponentName, "") // strips the .tsx or .component.tsx, etc from the end of the path
+    .replace(regexRouteMatchTripleDotSlug, "*") // replaces [...] with *
+    .replace(regexRouteMatchSlugName, ":$1"); // replaces [paramName] with :paramName
 
   const LayoutElement = lazy(() => lazyLoadPageWithLayout(page));
   return {
