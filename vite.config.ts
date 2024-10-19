3b63158e8f5c63b07a0ec77edd84f867dd5a5239
Jonathan Basniak <jon@bluedreamers.com>
2024-10-19 19:50:06 -0400

    chore: add vscode launch config

--- vite.config.ts
+++ vite.config.ts
@@ -13,15 +13,23 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    host: true,
+  },
   build: {
     target: "esnext",
     emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) return id.toString().split("node_modules/")[1].split("/")[0].toString();
-        }
-      }
-    }
+          if (id.includes("node_modules"))
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+        },
+      },
+    },
   },
 });
