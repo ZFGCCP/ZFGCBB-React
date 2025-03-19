5a41dd04420e0ded27b37c2663583f71d6a505f5
Jonathan Basniak <jon@bluedreamers.com>
2025-03-19 19:33:21 -0400

    chore: linkin park - in the end
    Signed-off-by: Jonathan Basniak <jon@bluedreamers.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { pigment } from "@pigment-css/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [pigment({}), reactRouter()],
+  plugins: [reactRouter(), pigment({})],
   build: {
     target: "esnext",
   },
