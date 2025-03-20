f465e953c0e038afa646b4ba7c9d6ffb76c20748
Jonathan Basniak <jon@bluedreamers.com>
2025-03-19 20:35:33 -0400

    chore: simplify vite config plugins and add start command for SSR in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,7 @@ import linaria from "@wyw-in-js/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    linaria({
-      // include: ["src/**/*.{ts,tsx}"],
-      // babelOptions: {
-      //   presets: ["@babel/preset-typescript", "@babel/preset-react"],
-      // },
-    }),
-    reactRouter(),
-  ],
+  plugins: [linaria(), reactRouter()],
   build: {
     target: "esnext",
   },
