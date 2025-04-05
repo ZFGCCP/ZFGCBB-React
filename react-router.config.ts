fc19202380e22f7de1c1b69cf7ee71065e861f07
Jonathan Basniak <jon@bluedreamers.com>
2025-04-05 03:08:27 -0400

    fix: add mapping of base_uri into react router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "src",
   ssr: false,
+  basename: process.env["CI"]
+    ? (process.env["VITE_BASE_URI"] ?? "/ZFGCBB-React/")
+    : "/",
 } satisfies Config;
