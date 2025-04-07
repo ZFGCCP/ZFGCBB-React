a4f675b63ccf83535957a40fe949b4ead16aecf4
Jonathan Basniak <jon@bluedreamers.com>
2025-04-07 09:15:19 -0400

    refactor: remove styled components from config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-// import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { generateImagePaths } from "./vite/plugins/vite-plugin-generate-image-paths";
@@ -16,14 +14,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: env["BASE_URL"],
-    plugins: [
-      react({
-        plugins: [["@swc/plugin-styled-components", {}]],
-      }),
-      tailwindcss(),
-      reactRouter(),
-      generateImagePaths(),
-    ],
+    plugins: [tailwindcss(), reactRouter(), generateImagePaths()],
     envPrefix: ["REACT_", "VITE_", "BASE_", "PROD"],
     build: {
       target: "esnext",
