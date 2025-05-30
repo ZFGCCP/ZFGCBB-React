8548f562ae32324a21b19341a0c5e168c4a3fdf6
Jonathan Basniak <jon@bluedreamers.com>
2025-05-30 11:56:19 -0400

    refactor: initial rework to tailwind

--- src/providers/theme/themeProvider.tsx
+++ src/providers/theme/themeProvider.tsx
@@ -1,48 +1,41 @@
 import type React from "react";
-import { useState, createContext, useContext } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
 import type { Theme, ThemeWrapper } from "../../types/theme";
 import { UserContext } from "../user/userProvider";
 
-const midnightTheme = {
-  borderWidth: ".2rem",
-  backgroundColor: "#204378",
-  widgetColor: "#1E2B44",
-  footerColor: "#132241",
-  headerColor: "#132241",
-  black: "black",
-  white: "white",
-  tableRow: "#1E2B44",
-  tableRowAlt: "#25334e",
-  textColor: "white",
-  linkColor: "white",
-  linkColorVisited: "white",
-} as Theme;
+const themeMap: Set<Theme> = new Set();
+themeMap.add("midnight");
 
-const themeMap: Map<String, Theme> = new Map();
-themeMap.set("midnight", midnightTheme);
-
-export const ThemeContext = createContext<ThemeWrapper>({
-  currentTheme: midnightTheme,
-});
+export const ThemeContext = createContext<ThemeWrapper>("midnight");
 
 const ThemeProvider: React.FC<{ children?: React.ReactNode }> = ({
   children,
 }) => {
   const { theme } = useContext(UserContext);
-  const [currentTheme, setCurrentTheme] = useState<ThemeWrapper>({
-    currentTheme: midnightTheme,
-  });
+  const [currentTheme, setCurrentTheme] = useState<ThemeWrapper>("midnight");
 
-  if (theme) {
-    const loadedTheme = themeMap.get(theme);
-    if (loadedTheme) {
-      setCurrentTheme({ currentTheme: loadedTheme });
+  useEffect(() => {
+    if (theme) {
+      const loadedTheme = themeMap.has(theme as Theme) ? theme : undefined;
+      if (loadedTheme) {
+        setCurrentTheme(loadedTheme);
+      }
     }
-  }
+
+    const className: `theme-${Theme}` = `theme-${theme ?? "midnight"}`;
+    document.body.classList.add(className);
+    import(`@/assets/themes/${theme ?? "midnight"}.css`).catch(console.error);
+
+    return () => {
+      document.body.classList.remove(className);
+    };
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={currentTheme}>
-      {children}
+      <div className={`theme-${theme ?? "midnight"} bg-background`}>
+        {children}
+      </div>
     </ThemeContext.Provider>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
-import react from "@vitejs/plugin-react-swc";
-// import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { generateImagePaths } from "./vite/plugins/vite-plugin-generate-image-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
@@ -15,13 +14,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: env["BASE_URL"],
-    plugins: [
-      react({
-        plugins: [["@swc/plugin-styled-components", {}]],
-      }),
-      reactRouter(),
-      generateImagePaths(),
-    ],
+    plugins: [reactRouter(), tailwindcss(), generateImagePaths()],
     envPrefix: ["REACT_", "VITE_", "BASE_", "PROD"],
     build: {
       target: "esnext",
