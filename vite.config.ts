9e829070813ecdc20cb16b80954a9870d18aa05b
Jonathan Basniak <jon@bluedreamers.com>
2025-07-11 18:20:27 -0400

    chore: tidying up CI to match CONTRIBUTING.md

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import { resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { generateImagePaths } from "./vite/plugins/vite-plugin-generate-image-paths";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const srcDirectory = resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ["REACT_", "VITE_", "BASE_"]);
-  process.env["VITE_MODE"] = mode;
-
+export default defineConfig(() => {
   return {
-    base: env["BASE_URL"],
-    plugins: [
-      // react({
-      //   plugins: [["@swc/plugin-styled-components", {}]],
-      // }),
-      reactRouter(),
-      generateImagePaths(),
-    ],
+    base: process.env["BASE_URL"] ?? "/",
+    plugins: [reactRouter(), generateImagePaths()],
     envPrefix: ["REACT_", "VITE_", "BASE_", "PROD"],
     build: {
       target: "esnext",
@@ -29,11 +21,12 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        "@": resolve(__dirname, "src"),
-        "~": resolve(__dirname, "src"),
+        "@": srcDirectory,
+        "~": srcDirectory,
       },
     },
     ssr: {
+      // Fixes build for styled-components.
       noExternal: ["styled-components"],
     },
   };
