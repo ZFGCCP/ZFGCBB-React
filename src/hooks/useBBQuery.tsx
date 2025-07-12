58c5fe440ddbc34d6d02ab32c8ebc88b5bfd50fd
Jonathan Basniak <jon@bluedreamers.com>
2025-07-12 17:49:18 -0400

    chore: reduce object creation

--- src/hooks/useBBQuery.tsx
+++ src/hooks/useBBQuery.tsx
@@ -10,24 +10,18 @@ export const useBBQuery = <T extends BaseBB | BaseBB[]>(
   staleTime: number = 100000,
   queryKey?: string,
 ) => {
-  const bbKey = queryKey ? queryKey : url;
-  const select = useCallback((data?: T) => data as T, []);
-
   return useQuery({
-    queryKey: [bbKey],
-    queryFn: async () => {
-      return await handleResponseWithJason<T>(
-        await fetch(`${import.meta.env.REACT_ZFGBB_API_URL}${url ?? "/"}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }),
-      );
-    },
-    retry: retry,
-    gcTime: gcTime,
+    queryKey: [queryKey ? queryKey : url],
+    queryFn: () =>
+      fetch(`${import.meta.env.REACT_ZFGBB_API_URL}${url ?? "/"}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => handleResponseWithJason<T>(response)),
+    retry,
+    gcTime,
     staleTime,
-    select,
+    select: useCallback((data?: T) => data as T, []),
   });
 };
