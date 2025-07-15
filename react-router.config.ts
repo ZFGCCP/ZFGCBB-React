4ecf2506683f39d7ac2822a8eef7024895333020
Jonathan Basniak <jon@bluedreamers.com>
2025-07-14 22:48:09 -0400

    chore: remove VITE_MODE env var

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,16 +1,19 @@
 import type { Config } from "@react-router/dev/config";
+import { parseArgs } from "node:util";
 import { loadEnv } from "vite";
-const env = loadEnv(process.env["VITE_MODE"] ?? "", process.cwd(), [
-  "REACT_",
-  "VITE_",
-]);
+
+const { mode } = parseArgs({
+  strict: false,
+  options: {
+    mode: { type: "string", default: "" },
+  },
+  allowPositionals: true,
+}).values;
+
+const env = loadEnv(`${mode}`, process.cwd(), ["REACT_", "VITE_"]);
 
 // FIXME: #113 fix: remove console.warn when BASE_URL is supported for local commands when react-router/dev fixes the issue with BASE_URL when middleware is enabled.
-if (
-  env["VITE_BASE"] &&
-  env["VITE_BASE"] !== "/" &&
-  process.env["VITE_MODE"] !== "github-pages"
-)
+if (env["VITE_BASE"] && env["VITE_BASE"] !== "/" && mode !== "github-pages")
   console.warn(
     `Setting VITE_BASE does not work for local "yarn preview" or "yarn preview:ssr" commands. Please use "yarn dev" instead.`,
   );
